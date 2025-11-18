bd7fe6ba2d5215275a4989a78bf694a3e8396307
Flavio-LP <flavio5070@outlook.com>
2025-11-17 22:27:57 -0300

    tests: Created tests for api

--- test/controllers/api/produtos_controller_test.rb
+++ test/controllers/api/produtos_controller_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
-class Api::ProdutosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class ProdutosApiTest < ActionDispatch::IntegrationTest
+  test "should get all produtos" do
+    get "/api/produtos"
+    assert_response :success
+    assert_equal "application/json; charset=utf-8", @response.content_type
+  end
+
+  test "should create produto" do
+    assert_difference("Produto.count", 1) do
+      post "/api/produtos",
+        params: { produto: { nome: "Teste", preco: 10.50, descricao: "Descrição teste" } },
+        as: :json
+    end
+    assert_response :created
+    json_response = JSON.parse(@response.body)
+    assert_equal "Teste", json_response["nome"]
+    assert_equal "10.5", json_response["preco"].to_s
+  end
+
+  test "should not create produto without nome" do
+    assert_no_difference("Produto.count") do
+      post "/api/produtos",
+        params: { produto: { preco: 10.50 } },
+        as: :json
+    end
+    assert_response :unprocessable_entity
+  end
 end
