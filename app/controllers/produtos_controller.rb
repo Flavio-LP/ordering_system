bff7a2b150912e814bf15aa50278616267b6d129
Flavio-LP <flavio5070@outlook.com>
2025-11-18 13:57:25 -0300

    feat: created pagination and search bar

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,18 +1,34 @@
 class ProdutosController < ApplicationController
   def index
-    @produtos = Produto.all
-  end
-
-  def new
-    @produto = Produto.new
+    respond_to do |format|
+      format.html
+      format.json do
+        page = params[:page]&.to_i || 1
+        per_page = params[:per_page]&.to_i || 10
+        offset = (page - 1) * per_page
+        
+        produtos = Produto.all
+        produtos = produtos.where("nome ILIKE ?", "%#{params[:search]}%") if params[:search].present?
+        
+        total_count = produtos.count
+        total_pages = (total_count.to_f / per_page).ceil
+        produtos = produtos.limit(per_page).offset(offset)
+        
+        render json: {
+          produtos: produtos,
+          total_pages: total_pages,
+          current_page: page
+        }
+      end
+    end
   end
 
   def create
     @produto = Produto.new(produto_params)
     if @produto.save
-      redirect_to produtos_path, notice: 'Produto criado com sucesso.'
+      render json: @produto, status: :created
     else
-      render :new
+      render json: @produto.errors, status: :unprocessable_entity
     end
   end
 
