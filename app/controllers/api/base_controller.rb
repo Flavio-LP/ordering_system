a90b9c74473cd8443f570c7918ce7af8166f4734
Flavio-LP <flavio5070@outlook.com>
2025-11-23 16:23:49 -0300

    ci: change verify_authenticity_token

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,6 +1,6 @@
 class Api::BaseController < ApplicationController
   before_action :authenticate_user!
-  skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session
 
   respond_to :json
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  protect_from_forgery with: :exception, unless: -> { request.format.json? }
-  protect_from_forgery with: :null_session, if: -> { request.format.json? }
 end
