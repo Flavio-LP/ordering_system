a9f38eb978d50a54d881c16eab2eebc5c7f2c9e2
Flavio-LP <flavio5070@outlook.com>
2025-12-18 15:43:47 -0300

    test: created test case with rspec and capybara to user login

---
+++ spec/system/User_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+require 'selenium-webdriver'
+
+
+RSpec.describe 'People registration', type: :system do
+
+    
+    people = {
+        nome: 'Usuario ',
+        sobrenome: '01',
+        empresa: 'empresa 01',
+        setor: 'setor 01'
+
+    }
+
+    #User.create!(
+    #    email: 'user@teste.com',
+    #    password: 'password123',
+    #    password_confirmation: 'password123'
+    #)
+
+    before do
+        driven_by(:selenium_chrome_headless)
+    end
+
+    it 'People registration without login', js: true do
+
+        post '/pessoas', params: people
+
+        expect(response).to have_http_status(:forbidden)
+    end
+
+    it 'People registration with login', js: true do
+
+        success = login(email: 'user@teste.com', password: 'password123')
+
+        expect(success).to be true
+
+    end
+
+    
+
+
+
+
+end
